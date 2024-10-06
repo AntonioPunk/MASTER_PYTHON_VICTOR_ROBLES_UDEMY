2114d601b0285b00f3245b602c112b26d18ac77f
Antonio Arias Ureta <kiosco.rock@hotmail.com>
2024-10-06 19:51:17 +0200

    Update

---
+++ 01-hola mundo/comentarios.py
@@ -0,0 +1,20 @@
+# Esto me muestra 'Hola' por pantalla
+print("Hola", end=" ")
+
+# Estas dos líneas me muestran 'mundo !!!' por pantalla
+print("mundo", end=" ")
+print("!!!")
+
+# print("Esta línea no se mostrará")
+
+""" Este es un comentario
+multilinea y sirve
+para hacer comentarios que
+ocupan varias líneas o para
+hacer que varias línea de código
+no se ejecuten."""
+
+"""
+print("Estas dos líneas", end=" ")
+print("no se mostrarán.")
+"""
